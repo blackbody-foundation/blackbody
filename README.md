3e597ff0686c198d68e84a74d6ec5f8bcefc1bf0
just-do-halee <just.do.halee@gmail.com>
2021-06-30 06:11:15 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+![BlackBody Image](https://i.ibb.co/4J0FkRr/crowdimage.png)
+
+# [BlackBody]() &middot; [![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://github.com/just-do-halee/blackbody/blob/main/LICENSE)
+
+
+Consensus compression blockchain.
+
+---
+
+> ㅤ
+>
+> - **blackbody:**  Blockchain node for consensus compression algorithm.
+>
+> - **blackhole:**  The Blackbody distributed deterministic data compressor/decompressor.
+>
+> -   **gravity:**  The Blackbody toolchain installer.
+>
+> -  **wormhole:**  A library for reading and writing .cccs file.
+> ㅤ
+>
---
+++ gravity/src/main.rs
@@ -0,0 +1,28 @@
+/*
+    (Source Code Name)
+
+    Copyright (C) 2021 Hwakyeom Kim(=just-do-halee)
+
+    BlackBody is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    BlackBody is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with BlackBody. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// gravity install (blackbody/blackhole) -net=(official/node)
+// gravity update -net=(official/node)
+// gravity (i/u) -o    (default official)
+// gravity (i/u) -n    (from node)
+
+fn main() {
+    println!("Hello, world!");
+}
